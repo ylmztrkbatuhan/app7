400418707aa1c49345572793adf0e551ba63b064
oboy21 <oboy11b@gmail.com>
2024-04-25 16:08:56 +0300

    Day 33: Filtering Forecast Data

---
+++ backend.py
@@ -0,0 +1,22 @@
+import requests
+
+
+API_KEY = "48ac4636a341adc1793132c3fb892780"
+
+
+def get_data(place, forecast_days=None, kind=None):
+    url =f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature" :
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    return filtered_data
+    if kind == "Sky":
+        filtered_data = [dict["weather"]["main"] for dict in filtered_data]
+
+    return filtered_data
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
