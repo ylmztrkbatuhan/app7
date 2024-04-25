a2558b00d17ee45fc9b1ff9e7cac89d4c695307f
oboy21 <oboy11b@gmail.com>
2024-04-25 19:19:32 +0300

    Day 33: The app finished

--- backend.py
+++ backend.py
@@ -11,12 +11,7 @@ def get_data(place, forecast_days=None, kind=None):
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature" :
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
     return filtered_data
-    if kind == "Sky":
-        filtered_data = [dict["weather"]["main"] for dict in filtered_data]
 
-    return filtered_data
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Temperature"))
+    print(get_data(place="Tokyo", forecast_days=3))
